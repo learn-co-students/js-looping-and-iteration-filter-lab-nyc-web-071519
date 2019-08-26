d27e81ecb708590b082f3649fa7bb90db05b7538
Andrew Santos <asantos@alumni.nd.edu>
2019-08-26 08:32:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+function findMatching(drivers, name){
+    const new_array = drivers.filter(function (driver) {return driver.toLowerCase() === name.toLowerCase();});
+    return new_array;
+}
+
+function fuzzyMatch(drivers, str){
+    const new_array = drivers.filter(function (driver) {return driver.startsWith(str);});
+    return new_array;
+}
+
+function matchName(drivers, string){
+    const new_array = drivers.filter(function (driver) {return driver[name] === name;});
+    return new_array;
+}
